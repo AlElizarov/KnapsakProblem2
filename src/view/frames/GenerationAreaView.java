4b617a645bd2a29f0d2d7dde08ad34f00c691864
Alexandr <Elizarov>
2016-05-17 12:35:49 +0300

    Загатовка для добавления генерации.

---
+++ src/view/frames/GenerationAreaView.java
@@ -0,0 +1,13 @@
+package view.frames;
+
+import viewmodel.TaskManager;
+
+public class GenerationAreaView {
+
+	private TaskManager manager;
+
+	public GenerationAreaView(TaskManager manager) {
+		this.manager = manager;
+	}
+
+}
